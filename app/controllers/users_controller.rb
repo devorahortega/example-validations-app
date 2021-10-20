9a0babe8d352b63b926ae29ae12616495537f6e2
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-20 15:31:09 -0400

    Creates update and create method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
+    
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages} status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find_by(:id params[:id])
+
+    first_name = params["first_name"]
+    last_name = params["last_name"]
+    email = params["email"]
+    user.save
+    render json: user
+  end
 end
